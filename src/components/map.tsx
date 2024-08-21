47eb40aa61ae506989325f33f96dddb9588cce85
Sagar Chand <chandsagar314@gmail.com>
2024-08-21 10:15:01 +0545

    set up project

---
+++ src/components/map.tsx
@@ -0,0 +1,15 @@
+import React, { useRef, useEffect } from "react";
+import * as maptilersdk from "@maptiler/sdk";
+import "@maptiler/sdk/dist/maptiler-sdk.css";
+
+const map = () => {
+  const mapContainer = useRef(null);
+  const map = useRef(null);
+  const ktm = { lng: 85.3240, lat: 27.7172 };
+  const zoom = 14;
+  maptilersdk.config.apiKey = "5KcqYQ9o1qNsAWH35Z8Y";
+
+  return <div></div>;
+};
+
+export default map;
