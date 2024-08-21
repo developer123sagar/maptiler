184e941a9c9d0f98431f06dba57f85ed376fe072
Sagar Chand <chandsagar314@gmail.com>
2024-08-21 10:23:59 +0545

    map rendered and css

--- src/components/map.tsx
+++ src/components/map.tsx
@@ -1,15 +1,30 @@
-import React, { useRef, useEffect } from "react";
+import { useRef, useEffect } from "react";
 import * as maptilersdk from "@maptiler/sdk";
 import "@maptiler/sdk/dist/maptiler-sdk.css";
 
 const Map = () => {
-  const mapContainer = useRef(null);
-  const map = useRef(null);
-  const ktm = { lng: 85.3240, lat: 27.7172 };
+  const mapContainer = useRef<HTMLDivElement>(null);
+  const map = useRef<maptilersdk.Map | null>(null);
+  const ktm = { lng: 85.324, lat: 27.7172 };
   const zoom = 14;
   maptilersdk.config.apiKey = "5KcqYQ9o1qNsAWH35Z8Y";
 
-  return <div></div>;
+  useEffect(() => {
+    if (map.current) return;
+
+    map.current = new maptilersdk.Map({
+      container: mapContainer.current!,
+      style: maptilersdk.MapStyle.STREETS,
+      center: [ktm.lng, ktm.lat],
+      zoom: zoom,
+    });
+  }, [ktm.lng, ktm.lat, zoom]);
+
+  return (
+    <div className="map-wrap">
+      <div ref={mapContainer} className="map" />
+    </div>
+  );
 };
 
 export default Map;
